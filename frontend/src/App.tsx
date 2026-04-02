a2a78012590f5fe028f2b6f944704e21deb9ea32
akhilmunnuru26 <munnuruakhil26@gmail.com>
2026-04-02 16:18:35 +0530

    removed dead code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Dashboard from "./pages/Dashboard";
 import Masters from "./pages/Masters";
